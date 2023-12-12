10a4d13012a9e711a95cddc48ba338e6bb90504f
NadiiaFedorovska <nadya.fedorovskaya@gmail.com>
2023-12-12 21:23:18 +0200

    HW 20 create project

---
+++ src/main/java/com/example/javaDevModule20/JavaDevModule20Application.java
@@ -0,0 +1,13 @@
+package com.example.javaDevModule20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevModule20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevModule20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaDevModule20/JavaDevModule20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaDevModule20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevModule20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
